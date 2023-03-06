7a346ee6e00a069db94a10e3dfad9a4cc0ad0c41
ahmedgavar <ahmedgavar@yahoo.com>
2023-03-06 16:58:34 +0200

    modify for no time

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUPForm,
   },
   {
     path: "/about",
---
+++ src/views/Auth/Signup.vue
@@ -0,0 +1,14 @@
+<template>
+  <SignUpForm></SignUpForm>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import "@/components/Auth/SignUpForm.vue"
+  export default defineComponent ({
+   
+    
+  })
+</script>
+<style lang="scss" scoped>
+</style>
