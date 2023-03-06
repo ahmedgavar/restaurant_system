ff27d098a7bd25b38d9b5ad6fb3b674c8451106f
ahmedgavar <ahmedgavar@yahoo.com>
2023-03-06 14:46:45 +0200

    format code with eslint

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "normalize.css";
